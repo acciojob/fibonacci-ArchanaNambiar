357e3c210420da748f35df9ffb2e9e98f05eaa59
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-07-18 14:35:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 function fibonacci(num) {
 // your code here
-	if (num === 0) {
-    return 0;
-  } else if (num === 1 || num === 2) {
-    return 1;
-  } else if (num > 50) {
-    return "Number too large. Please provide a number between 0 and 50.";
-  }
+	let n1 = 0, n2 = 1, nextTerm;
+for (let i = 1; i <= num; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
 
-  let a = 0;
-  let b = 1;
-  let result = 0;
-
-  for (let i = 2; i <= num; i++) {
-    result = a + b;
-    a = b;
-    b = result;
-  }
-
-  return result;
+ 
+  
 }
 
 module.exports = fibonacci;
