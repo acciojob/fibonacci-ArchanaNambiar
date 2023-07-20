8198d6e0b8e3a25ea09f8554c3fb50b3ced7ae5f
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-07-20 23:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function fibonacci(num) {
 // your code here
-	let n1 = 0, n2 = 1, nextTerm;
-for (let i = 1; i <= num; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
+	let n1 = 0, n2 = 1;
+	if(num==1) return n2;
+	if(num==2) return n1;
+for (let i = 3; i <= num; i++) 
+{
+    
+   let nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
-
+}
+	return n2;
  
   
 }
